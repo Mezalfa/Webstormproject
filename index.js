c1faf1c491d0319497515a74f61dde9a7af6b367
Mesmer Gebrealfa <mgebrealfa@fordham.edu>
2025-10-05 13:32:33 -0700

    Initial commit with working local server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000; // You can pick any available port
+
+app.get('/', (req, res) => {
+    res.send('Hello World from Express!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
